bf7a4240bec6e7341c55fd3b03baf1f9d985b1fb
haziqhalifi <haziq.halifi@gmail.com>
2025-02-08 08:09:00 +0800

    Add banner upload functionality and animation wrapper to blog editor

--- blogging website - frontend/src/components/blog-editor.component.jsx
+++ blogging website - frontend/src/components/blog-editor.component.jsx
@@ -1,7 +1,15 @@
 import { Link } from "react-router-dom";
 import image from "../imgs/logo.png";
+import AnimationWrapper from "../common/page-animation";
+import defaultBanner from "../imgs/blog banner.png";
 
 const BlogEditor = () => {
+  const handleBannerUpload = (e) => {
+    console.log(e);
+    let img = e.target.files[0];
+    console.log(img);
+  };
+
   return (
     <>
       <nav className="navbar">
@@ -16,6 +24,25 @@ const BlogEditor = () => {
           <button className="btn-light py-2">Save Draft</button>
         </div>
       </nav>
+
+      <AnimationWrapper>
+        <section>
+          <div className="mx-auto max-w-[900px] w-full">
+            <div className="relative aspect-video hover:opacity-80 bg-white border-4 border-grey">
+              <label>
+                <img src={defaultBanner} className="z-20" />
+                <input
+                  id="uploadBanner"
+                  type="file"
+                  accept=".png, .jpg, .jpeg"
+                  hidden
+                  onChange={handleBannerUpload}
+                ></input>
+              </label>
+            </div>
+          </div>
+        </section>
+      </AnimationWrapper>
     </>
   );
 };
