2bc98f642bd9c83b16ad52e627b4b9f824ea937f
Aviral <jamss0112@gmail.com>
2025-02-28 14:35:49 +0530

    Screen color change

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
 
   return (
   
-      <div className='m-3 pt-5 px-1 bg-white'>
+      <div className='m-3 pt-5 px-1 bg-white text-black h-screen'>
         <h1 className='flex py-5 font-bold italic'>My Blog</h1>
         <div className='m-5 p-5 absolute top-5 end-0'>
         <button onClick={()=> setIsAdded(!isAdded)} className='bg-amber-600  size-16 rounded-[100%] flex justify-center items-center shadow-sm hover:shadow-lg hover:border-2 border-amber-700'>Add</button>
