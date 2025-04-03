89e0949694a2df27ec7a8151cb72c6f41b099c96
Anu17shka <anushkagoyal42@gmail.com>
2025-04-04 03:09:03 +0530

    sign-in page update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import Navbar from "./components/navbar.component"
 import {Routes, Route} from "react-router-dom"
+import UserAuthForm from "./pages/userAuthForm.page";
 
 
 const App = () => {
     return (
         <Routes>
         <Route path="/" element={<Navbar />}>
-        <Route path="signin" element={<h1>sign in page</h1>} />
-         <Route path="signup" element={<h1>sign up page</h1>} />
+        <Route path="signin" element={<UserAuthForm type="sign-in"/>} />
+         <Route path="signup" element={<UserAuthForm type="sign-up" />} />
         </Route>
         
         </Routes>
